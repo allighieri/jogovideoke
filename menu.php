58c1d626f64c961500a368ded381e38e11469dd4
Weder Monteiro <agenciaolhardigital@gmail.com>
2017-03-16 01:16:16 -0300

    quebra de linha corrigida no menu

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-<a href="gerargrupos.php">Gerar Grupos</a></br>
-<a href="geraduplas.php">Gerar Duplas</a></br>
-<a href="alternaduplas.php">Alterna Duplas</a></br>
-<a href="gerafinal.php">Gera Final</a></br>
+<a href="gerargrupos.php">Gerar Grupos</a><br />
+<a href="geraduplas.php">Gerar Duplas</a><br />
+<a href="alternaduplas.php">Alterna Duplas</a><br />
+<a href="gerafinal.php">Gera Final</a><br />
